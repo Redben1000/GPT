48415dd7d9711738d766eb2802dd21324c494839
Gamerboy88 <110696589+Gamerboy199@users.noreply.github.com>
2023-04-26 18:51:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-  return render_template("index.html")
-
-@app.route("/chatbot/<prompt>")
 def chatbot(prompt):
-  response = openai.Completion.create(model="text-davinci-003",
-                                      prompt=prompt,
-                                      n=1,
-                                      temperature=0.6,
-                                      max_tokens=2500,
-                                      stop=None)
-  return response["choices"][0]['text']
+    response = openai.Completion.create(model="text-davinci-003",
+                                        prompt=prompt,
+                                        n=1,
+                                        temperature=0.6,
+                                        max_tokens=2500,
+                                        stop=None)
+    return response["choices"][0]['text']
 
-if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        message = request.form['message']
+        AI_response = chatbot(message)
+        return render_template('home.html', AI_response=AI_response)
+    else:
+        return render_template('home.html', AI_response='')
+
+if __name__ == '__main__':
+    app.run(debug=True)
